64708745b0a1847a34bfd04fb5fcaf30a3a7c1bd
s-ieremenko <ieremenko.sveta@gmail.com>
2021-07-08 12:06:10 +0300

    Added package.json, moved constants to the separate file, added constants for encoding, status codes and content-type in order to avoid duplicating, chenged spaces to 2, re-wrote if-else statements

--- app.ts
+++ app.ts
@@ -1,89 +1,94 @@
-import { createServer, IncomingMessage, ServerResponse } from 'http';
+import {
+  createServer,
+  IncomingMessage,
+  ServerResponse,
+  OutgoingHttpHeaders,
+} from 'http';
 import * as fs from 'fs';
-
-const host: string = '127.0.0.1';
-const port: number = 3000;
-const path: string = './newFile.txt';
+import {
+  host,
+  port,
+  path,
+  enc,
+  contentTypeMime,
+  ok,
+  notFound,
+  serverError,
+  methodNotAllowed,
+  gone,
+} from './constans';
 
 createServer((request: IncomingMessage, response: ServerResponse) => {
-    switch (request.method) {
-        case 'GET': {
-            fs.readFile(path, 'utf-8', (error, data) => {
-                if (error) {
-                    response.writeHead(404, { 'Content-Type': 'text/plain' })
-                    response.end('Resourse not found!');
-                }
-                else {
-                    response.writeHead(200, { 'Content-Type': 'text/plain' })
-                    response.end(data);
-                }
-            })
-            break;
+  switch (request.method) {
+    case 'GET': {
+      fs.readFile(path, enc, (error, data) => {
+        if (error) {
+          response.writeHead(notFound, contentTypeMime);
+          return response.end('Resourse not found!');
         }
-        case 'POST': {
-            let body: string = '';
-            request.on('data', chunk => {
-                body += chunk;
-            })
-            request.on('end', () => {
-                fs.writeFile(path, body += '\n', err => {
-                    if (err) {
-                        response.writeHead(500, { 'Content-Type': 'text/plain' })
-                        response.end('Server error!');
-                    } else {
-                        response.writeHead(200, { 'Content-Type': 'text/plain' })
-                        response.end('File is written');
-                    }
-                })
-            })
-            break;
-        }
-        case 'DELETE': {
-            fs.unlink(path, error => {
-                if (error) {
-                    response.writeHead(410, { 'Content-Type': 'text/plain' })
-                    response.end('Gone');
-                } else {
-                    response.writeHead(200, { 'Content-Type': 'text/plain' })
-                    response.end(`${path.substring(2)} was deleted`)
-                }
-            });
-            break;
-        }
-        case 'PATCH': {
-            fs.access(path, fs.constants.R_OK, err => {
-                if (err) {
-                    response.writeHead(404, { 'Content-Type': 'text/plain' })
-                    response.end("Resourse not found!");
-
-                } else {
-                    let body: string = '';
-                    request.on('data', chunk => {
-                        body += chunk;
-                    })
-                    request.on('end', () => {
-                        fs.appendFile(path, body += '\n', err => {
-                            if (err) {
-                                response.writeHead(500, { 'Content-Type': 'text/plain' })
-                                response.end('Internal Server Error');
-                            } else {
-                                response.writeHead(200, { 'Content-Type': 'text/plain' })
-                                response.end('File is written');
-                            }
-                        }
-                        )
-                    })
-                }
-            })
-            break;
-        }
-
-        default: {
-            response.writeHead(405, { 'Content-Type': 'text/plain' })
-            response.end('Method Not Allowed');
-            break
-        }
+        response.writeHead(ok, contentTypeMime);
+        return response.end(data);
+      });
+      break;
     }
+    case 'POST': {
+      let body: string = '';
+      request.on('data', (chunk) => {
+        body += chunk;
+      });
+      request.on('end', () => {
+        fs.writeFile(path, (body += '\n'), (err) => {
+          if (err) {
+            response.writeHead(serverError, contentTypeMime);
+            return response.end('Server error!');
+          }
+          response.writeHead(ok, contentTypeMime);
+          return response.end('File is written');
+        });
+      });
+      break;
+    }
+    case 'DELETE': {
+      fs.unlink(path, (error) => {
+        if (error) {
+          response.writeHead(gone, contentTypeMime);
+          return response.end('Gone');
+        }
+        response.writeHead(ok, contentTypeMime);
+        return response.end(`${path.substring(2)} was deleted`);
+      });
+      break;
+    }
+    case 'PATCH': {
+      fs.access(path, fs.constants.R_OK, (err) => {
+        if (err) {
+          response.writeHead(notFound, contentTypeMime);
+          return response.end('Resourse not found!');
+        }
+        let body: string = '';
+        request.on('data', (chunk) => {
+          body += chunk;
+        });
+        request.on('end', () => {
+          fs.appendFile(path, (body += '\n'), (err) => {
+            if (err) {
+              response.writeHead(serverError, contentTypeMime);
+              return response.end('Internal Server Error');
+            }
+            response.writeHead(ok, contentTypeMime);
+            return response.end('File is written');
+          });
+        });
+      });
+      break;
+    }
+
+    default: {
+      response.writeHead(methodNotAllowed, contentTypeMime);
+      response.end('Method Not Allowed');
+      break;
+    }
+  }
 }).listen(port, host, () => {
-    console.log(`Server listens http://${host}:${port}`)
-})
+  console.log(`Server listens http://${host}:${port}`);
+});
